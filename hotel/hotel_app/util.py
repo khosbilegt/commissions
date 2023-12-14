601e4f0903c0baaeaef60556ea61a969f13f8230
khosbilegt.b <khosbilegt.b@unitel.mn>
2023-12-14 11:04:23 +0800

    fixed booking logic

--- hotel/hotel_app/util.py
+++ hotel/hotel_app/util.py
@@ -1,56 +1,26 @@
 from datetime import datetime, timedelta
+from django.db import transaction
 from hotel_app.models import Room, RoomBooking, User
 
 def process_dates_string(user_id, room_id, dates_string):
     date_list = dates_string.split("|")
     date_list.pop()
-    print(date_list)
 
-    bookings = []
-    current_group = [datetime.strptime(date_list[0], "%Y/%m/%d")]
+    # Convert date strings to datetime objects
+    date_objects = [datetime.strptime(date, "%Y/%m/%d") for date in date_list]
 
-    for i in range(0, len(date_list)):
-        print(date_list[i])
-        current_date = datetime.strptime(date_list[i], "%Y/%m/%d")
-        previous_date = current_group[-1] + timedelta(days=1)
+    # Find the start and end dates
+    start_date = date_objects[0]
+    end_date = date_objects[-1] + timedelta(days=1)
 
-        if current_date == previous_date:
-            current_group.append(current_date)
-        else:
-            # Process the current group of consecutive dates
-            start_date = current_group[0]
-            end_date = current_group[-1] + timedelta(days=1)
-
-            # Create a booking for the current group
-            room_booking = RoomBooking.objects.create(
-                room_id=room_id,
-                user_id=user_id,
-                start_date=start_date.replace(hour=13, minute=0, second=0, microsecond=0),
-                end_date=end_date.replace(hour=13, minute=0, second=0, microsecond=0)
-            )
-            room_booking.save()
-
-            # Add the booking to the list
-            bookings.append(room_booking.booking_id)
-
-            # Reset the current group for the next consecutive dates
-            current_group = [current_date]
-
-    # Process the last group if it exists
-    if current_group:
-        start_date = current_group[0]
-        end_date = current_group[-1] + timedelta(days=1)
-
-        # Create a booking for the last group
+    with transaction.atomic():
+        # Create a single booking for the entire range
         room_booking = RoomBooking.objects.create(
             room_id=room_id,
             user_id=user_id,
             start_date=start_date.replace(hour=13, minute=0, second=0, microsecond=0),
-            end_date=end_date.replace(hour=13, minute=0, second=0, microsecond=0)
+            end_date=end_date.replace(hour=13, minute=0, second=0, microsecond=0),
+            is_received=False  # Assuming the default value for is_received is False
         )
-        room_booking.save()
 
-        # Add the booking to the list
-        bookings.append(room_booking.booking_id)
-
-    return bookings
+    return room_booking.booking_id
